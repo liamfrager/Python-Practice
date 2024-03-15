15e76936f89001afe71ae1426e774d2d9db79bda
Liam Frager <liam.frager@gmail.com>
2024-03-14 21:35:00 -0400

    Day 55 final

--- 54 Flask Intro/main.py
+++ 54 Flask Intro/main.py
@@ -3,15 +3,35 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def bold(func):
+    def wrap():
+        return f"<b>{func()}</b>"
+    return wrap
+
+
+def italic(func):
+    def wrap():
+        return f"<em>{func()}</em>"
+    return wrap
+
+
+def underline(func):
+    def wrap():
+        return f"<u>{func()}</u>"
+    return wrap
+
+
 @app.route('/')
+@bold
+@italic
 def hello_world():
     return 'Hello, World!'
 
 
 @app.route('/test')
 def test():
-    return '<h1>Test<h1>'
+    return 'Test'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
