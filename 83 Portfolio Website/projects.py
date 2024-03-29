fae15b82b0a8cd0d277ac8b11be466bdb5ab357e
Liam Frager <liam.frager@gmail.com>
2024-03-29 15:26:07 -0400

    Update website with Day 85 project

--- 83 Portfolio Website/projects.py
+++ 83 Portfolio Website/projects.py
@@ -3,7 +3,7 @@ coding_projects = [
     {
         'id': 0,
         'title': 'Morse Code Translator',
-        'description': 'I did this project and it does a thing.',
+        'description': 'A program that converts any user inputed text into morse code.',
         'img_url': 'https://cdn.britannica.com/00/192200-050-8847FCB9/key-telegraph-Morse-1860.jpg',
         'language': 'Python',
         'completion_date': date(2024, 3, 27),
@@ -12,10 +12,19 @@ coding_projects = [
     {
         'id': 1,
         'title': 'Tic-Tac-Toe',
-        'description': 'A bog standard game of Tic-Tac-Toe.',
+        'description': 'A text based game of Tic-Tac-Toe playable in the console.',
         'img_url': 'https://cdn-icons-png.freepik.com/512/8002/8002181.png',
         'language': 'Python',
-        'completion_date': date(2024, 3, 27),
-        'git_endpoint': '82%20Morse%20Code%20Converter',
-    }
+        'completion_date': date(2024, 3, 28),
+        'git_endpoint': '84%20Tic-Tac-Toe',
+    },
+    {
+        'id': 2,
+        'title': 'Watermark Applicator',
+        'description': 'A desktop application with a GUI that allows users to add a watermark to any image they upload.',
+        'img_url': 'https://t3.ftcdn.net/jpg/05/32/81/40/360_F_532814019_0vVdfSlMafbN4xbMrya7AuwJWIFw5AXD.jpg',
+        'language': 'Python',
+        'completion_date': date(2024, 3, 29),
+        'git_endpoint': '85%20Watermark%20Applicator',
+    },
 ]
