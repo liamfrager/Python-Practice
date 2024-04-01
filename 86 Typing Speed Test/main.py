a389a672150da97757b1c63d16cf5a100cd24b6c
Liam Frager <liam.frager@gmail.com>
2024-04-01 11:52:52 -0400

    Day 86 final

--- 86 Typing Speed Test/main.py
+++ 86 Typing Speed Test/main.py
@@ -4,17 +4,15 @@ from common_words import random_paragraph
 import time
 
 BG_COLOR = 'lightblue'
-FONT = 'Courier'
-TIMER_LENGTH = 3
+HIGHLIGHT_COLOR = 'yellow'
+CORRECT_COLOR = 'green'
+ERROR_COLOR = 'red'
+FONT_COLOR = 'white'
+FONT = ('Courier', 20, 'bold')
+TIMER_LENGTH = 60
 
 
-class Timer:
-
-    def time_stop(self):
-        return  # Time
-
-
-class GUI(tk.Tk, Timer):
+class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
         # GUI
@@ -22,24 +20,28 @@ class GUI(tk.Tk, Timer):
         self.config(padx=30, pady=30, bg=BG_COLOR)
         self.heading = tk.Label(
             text='Typing Speed Test',
-            font=(FONT, 40, 'bold'),
+            font=('Courier', 40, 'bold'),
             bg=BG_COLOR,
-            fg='white'
+            fg=FONT_COLOR
         ).pack()
         # Start message
         self.start_msg = tk.Text(
             wrap='word',
-            font=(FONT),
+            font=FONT,
             bg=BG_COLOR,
-            fg='white',
-            highlightbackground=BG_COLOR
+            fg=FONT_COLOR,
+            highlightbackground=BG_COLOR,
+            height=3
         )
         self.start_msg.insert(
             tk.END,
-            '''Welcome to the Speed Typing Test!\nThe goal is to type as many words correctly as you can in one minute.\nPress the spacebar to begin.'''
+            f"Welcome to the Speed Typing Test!\n"
+            f"The goal is to type as many words correctly "
+            f"as you can in {TIMER_LENGTH} seconds.\n"
+            f"Press the spacebar to begin."
         )
-        self.start_msg.tag_add("center", "1.0", "end")
-        self.start_msg.tag_configure("center", justify='center')
+        self.start_msg.tag_add('center', '1.0', 'end')
+        self.start_msg.tag_configure('center', justify='center')
         self.start_msg.config(state='disabled')
         # End message
         self.end_frame = tk.Frame(self)
@@ -47,29 +49,28 @@ class GUI(tk.Tk, Timer):
         self.stats = tk.Label(
             self.end_frame,
             text='',
-            font=(FONT),
+            font=FONT,
             bg=BG_COLOR,
-            fg='white'
+            fg=FONT_COLOR
         )
         self.stats.pack(pady=2)
         self.play_again = tk.Label(
             self.end_frame,
-            text=f'Would you like to play again?',
-            font=(FONT),
+            text=f"Would you like to play again?",
+            font=FONT,
             bg=BG_COLOR,
-            fg='white'
+            fg=FONT_COLOR
         )
         self.play_again.pack(pady=2)
         self.button = tk.Button(
             self.end_frame,
-            text='Play again',
-            font=(FONT),
+            text="Play again",
+            font=FONT,
             highlightbackground=BG_COLOR,
             command=self.reset
         )
         self.button.pack(pady=2)
-        ##########
-        self.cpm = 0
+        # Begin
         self.show_start_msg()
         self.mainloop()
 
@@ -78,67 +79,109 @@ class GUI(tk.Tk, Timer):
         # Variables
         self.char_index = 0
         self.word_index = 0
-        self.wordstart = 0
+        self.word_start = 0
+        self.typed_char = 0
+        self.errors = 0
         self.bind('<space>', self.start_test)
 
     def start_test(self, event):
         self.start_msg.pack_forget()
         self.timer_display = tk.Label(
             text='60:00',
-            font=(FONT),
+            font=FONT,
             bg=BG_COLOR,
-            fg='white'
+            fg=FONT_COLOR
         )
         self.timer_display.pack()
-        self.test_text = random_paragraph(300).split()
+        self.test_text = random_paragraph(150).split()
         self.textbox = tk.Text(
             wrap='word',
-            font=(FONT),
-            height=25,
+            font=FONT,
+            height=12,
             bg='white',
             fg='black'
         )
         self.textbox.insert(tk.END, self.test_text)
         self.textbox.config(state='disabled')
         self.textbox.pack(pady=5)
+        self.set_styling()
         self.highlight_word()
+        self.current_word = ''
         self.unbind('<space>')
         self.timer()
 
     def on_key(self, event):
         if event.keysym == 'BackSpace':
-            if self.char_index != self.wordstart:
-                self.char_index -= 1
-                self.textbox.tag_delete(
-                    'char' + str(self.char_index)
-                )
+            self.on_backspace()
         elif event.keysym == 'space' and self.char_index != 0:
-            self.char_index = self.wordstart + \
-                len(self.test_text[self.word_index]) + 1
-            self.wordstart = self.char_index
-            self.word_index += 1
-            self.textbox.tag_delete(
-                'word' + str(self.word_index - 1)
-            )
-            self.highlight_word()
+            self.on_space()
         elif len(event.keysym) == 1:
+            self.on_letter(event.keysym)
+
+    def on_backspace(self):
+        if self.char_index != self.word_start:
+            self.char_index -= 1
+            self.typed_char -= 1
+            self.current_word = self.current_word[:-1]
+            deleting = self.textbox.tag_names(f'1.{self.char_index}')[1]
+            if deleting == 'error':
+                self.errors -= 1
+            self.textbox.tag_remove(
+                deleting,
+                f'1.{self.char_index}'
+            )
+
+    def on_space(self):
+        if self.char_index < self.word_end:
             self.textbox.tag_add(
-                'char' + str(self.char_index),
-                f"1.{self.char_index}"
+                'error',
+                f'1.{self.char_index}',
+                f'1.{self.word_end}'
             )
-            self.textbox.tag_configure(
-                'char' + str(self.char_index), foreground='green' if event.keysym == self.textbox.get(f"1.{self.char_index}") else 'red'
-            )
-            self.char_index += 1
+            self.errors += 1
+        self.textbox.tag_remove(
+            'highlight', f'1.{self.word_start}', f'1.{
+                self.word_end}'
+        )
+        self.current_word = ''
+        self.char_index = self.word_end + 1
+        self.word_start = self.char_index
+        self.word_index += 1
+        self.typed_char += 1
+        self.highlight_word()
+
+    def on_letter(self, letter):
+        if self.char_index == self.word_end:
+            self.on_backspace()
+        self.current_word += letter
+        if letter == self.textbox.get(f'1.{self.char_index}'):
+            tag = 'correct'
+        else:
+            tag = 'error'
+            self.errors += 1
+        self.textbox.tag_add(
+            tag,
+            f'1.{self.char_index}'
+        )
+        self.char_index += 1
+        self.typed_char += 1
 
     def highlight_word(self):
         self.textbox.tag_add(
-            'word' + str(self.word_index),
-            f"1.{self.char_index}",
-            f"1.{self.char_index + len(self.test_text[self.word_index])}",
+            'highlight',
+            f'1.{self.word_start}',
+            f'1.{self.word_end}',
+        )
+
+    def set_styling(self):
+        self.textbox.tag_configure(
+            'highlight', background=HIGHLIGHT_COLOR
         )
         self.textbox.tag_configure(
-            'word' + str(self.word_index), background='yellow'
+            'correct', foreground=CORRECT_COLOR
+        )
+        self.textbox.tag_configure(
+            'error', foreground=ERROR_COLOR
         )
 
     def timer(self):
@@ -147,28 +190,34 @@ class GUI(tk.Tk, Timer):
         while datetime.now().timestamp() < self.start_time + TIMER_LENGTH:
             self.update()
             time.sleep(.01)
-            elapsed_time = '{:.2f}'.format(
-                round(TIMER_LENGTH - (datetime.now().timestamp() - self.start_time), 2)).replace(".", ":")
+            elapsed_time = '{:.2f}'.format(round(
+                TIMER_LENGTH - (datetime.now().timestamp() - self.start_time), 2)).replace(".", ":")
             self.timer_display.config(text=elapsed_time)
-            print(elapsed_time)
-        self.timer_display.config(text='Times up!')
+        self.timer_display.config(text="Time's up!")
         self.unbind('<KeyRelease>')
         self.show_end_msg()
 
     def show_end_msg(self):
         self.textbox.pack_forget()
-        self.calculate_score()
+        self.cpm = int(self.typed_char // (TIMER_LENGTH / 60)) + 1
+        self.wpm = int(((self.typed_char / 5) - self.errors) //
+                       (TIMER_LENGTH / 60)) + 1
         self.stats.config(
-            text=f'Your typing stats: {self.cpm} CPM, {self.cpm // 5} WPM'
+            text=f"Your typing stats: "
+            f"{self.cpm} CPM, "
+            f"{self.errors} ERR, "
+            f"{self.wpm} WPM"
         )
         self.end_frame.pack()
 
-    def calculate_score(self):
-        pass
-
     def reset(self):
         self.end_frame.pack_forget()
+        self.timer_display.pack_forget()
         self.show_start_msg()
+
+    @property
+    def word_end(self):
+        return self.word_start + len(self.test_text[self.word_index])
 
 
 app = GUI()
