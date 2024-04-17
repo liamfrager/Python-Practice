87ccc31e251023e6dc1a71372b304fab22cf069e
Liam Frager <liam.frager@gmail.com>
2024-04-17 13:06:24 -0400

    Day 92 final

--- 92 Image Color Pallete Generator/main.py
+++ 92 Image Color Pallete Generator/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request
 from PIL import Image
-import numpy as np
+from io import BytesIO
 import requests as req
+from base64 import b64encode
 
 app = Flask(__name__)
 
@@ -9,20 +10,38 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        if request.form['url'] != '':
-            res = req.get(request.form['image_url'])
-            print(res.content)
-        else:
-            image = request.files['image_file']
-        img = Image.open(image)
-        paletted = img.convert('P', palette=Image.ADAPTIVE, colors=10)
-        color_indeces = sorted(paletted.getcolors(
-            img.width * img.height), reverse=True)
-        palette = paletted.getpalette()
-        print(palette)
-        colors = ['#%02x%02x%02x' % tuple(palette[index[1]*3:index[1]*3+3])
-                  for index in color_indeces]
-        return render_template('index.html', image=image, colors=colors)
+
+        image = ''
+
+        # Get image bytes from url/file
+        if request.form['image_url'] != '':
+            try:
+                res = req.get(request.form['image_url'])
+                res.raise_for_status()
+                image = BytesIO(res.content)
+            except req.HTTPError:
+                error = f'{res.status_code}: Invalid image URL'
+                return render_template('index.html', error=error)
+            except Exception:
+                error = 'Invalid image URL'
+                return render_template('index.html', error=error)
+        elif 'image_file' in request.files:
+            image = BytesIO(request.files['image_file'].read())
+
+        # Get most prominent colors
+        try:
+            print(request.files['image_file'].filename)
+            img = Image.open(image)
+            paletted = img.convert('P', palette=Image.ADAPTIVE, colors=10)
+            color_indeces = sorted(paletted.getcolors(
+                img.width * img.height), reverse=True)
+            palette = paletted.getpalette()
+            colors = ['#%02x%02x%02x'.title() % tuple(palette[index[1]*3:index[1]*3+3])
+                      for index in color_indeces]
+            return render_template('index.html', image='data:image/png;base64,' + b64encode(image.getvalue()).decode('ascii'), colors=colors)
+        except:
+            return render_template('index.html', error='Could not open image')
+
     elif request.method == 'GET':
         return render_template('index.html')
 
