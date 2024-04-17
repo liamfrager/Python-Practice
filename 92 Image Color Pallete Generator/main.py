cf104720a90a25e9ea53de1f43aad4ee52cd59bb
Liam Frager <liam.frager@gmail.com>
2024-04-17 13:09:03 -0400

    Day 92 final, remove unecessary imports

--- 92 Image Color Pallete Generator/main.py
+++ 92 Image Color Pallete Generator/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request
 from PIL import Image
 from io import BytesIO
 import requests as req
