065f8f52d8fda9be986952220d8219700322af50
Liam Frager <liam.frager@gmail.com>
2024-04-20 14:11:30 -0400

    Day 94 final

--- 94 Google Dino Game Automation/main.py
+++ 94 Google Dino Game Automation/main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from PIL import ImageGrab
 import pyautogui as gui
-import time
+import math
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -15,17 +15,27 @@ gui.press('up')
 
 jumps = 0
 while True:
-    # time.sleep(.01)
-    area = ImageGrab.grab((650, 380, 800, 400)).convert('L')
-    pixels = area.load()
+    game_over = ImageGrab.grab((820, 332, 850, 333)).convert('L').load()
+    for i in range(30):
+        if game_over[i, 0] < 100:
+            gui.press('up')
+            jumps = 0
+            break
 
-    for i in range(50 + jumps if 50 + jumps < 70 else 70):
-        top_pixel = pixels[i, 19]
-        bot_pixel = pixels[i, 0]
+    # Not a great algorithm, but I tried.
+    jump_if_obstacle_range = int(round(90 + jumps ** 1.7 * .1))
+    jump_if_obstacle_range = jump_if_obstacle_range if jump_if_obstacle_range < 200 else 200
+    screenshot = ImageGrab.grab(
+        (610, 380, 610 + jump_if_obstacle_range, 400)
+    ).convert('L').load()
+
+    for i in range(jump_if_obstacle_range):
+        top_pixel = screenshot[jump_if_obstacle_range - i - 1, 19]
+        bot_pixel = screenshot[jump_if_obstacle_range - i - 1, 0]
         if top_pixel < 100 or bot_pixel < 100:
             gui.press('up')
             jumps += 1
             break
 
 
-driver.quit()
+# HIGH SCORE: 1047
