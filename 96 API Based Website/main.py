49fa35dc76293859abf5c8c59115de7e1043b1f4
Liam Frager <liam.frager@gmail.com>
2024-04-28 21:53:28 -0400

    Day 96 add house cards, add house pages

--- 96 API Based Website/main.py
+++ 96 API Based Website/main.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    houses = {
+        'gryffindor': {'name': 'Gryffindor', 'img': 'https://static.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png', 'qualities': ['courageous', 'brave', 'determined'], 'colors': ['firebrick', 'gold']},
+        'ravenclaw': {'name': 'Ravenclaw', 'img': 'https://static.wikia.nocookie.net/pottermore/images/4/4f/Ravenclaw_crest.png', 'qualities': ['wise', 'witty', 'observant'], 'colors': ['deepskyblue', 'sienna']},
+        'hufflepuff': {'name': 'Hufflepuff', 'img': 'https://static.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png', 'qualities': ['loyal', 'humble', 'patient'], 'colors': ['gold', 'black']},
+        'slytherin': {'name': 'Slytherin', 'img': 'https://static.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png', 'qualities': ['ambitious', 'cunning', 'proud'], 'colors': ['forestgreen', 'silver']},
+    }
+    return render_template('index.html', houses=houses)
+
+
+@app.route('/<house>')
+def house(house):
+    return render_template('house.html', house=house)
 
 
 if __name__ == '__main__':
