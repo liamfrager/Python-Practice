f676b14d5e196974e3ed886f40b496ff4cc367b5
Liam Frager <liam.frager@gmail.com>
2024-05-02 22:36:24 -0400

    Day 96 revamp concept to battle app

--- 96 API Based Website/main.py
+++ 96 API Based Website/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, make_response, redirect, url_for, request
 import requests as req
 
 app = Flask(__name__)
@@ -13,23 +13,50 @@ houses = {
 
 @app.route('/')
 def home():
-    return render_template('index.html', houses=houses)
+    return render_template('index.html')
 
 
-@app.route('/<house>')
-def house(house):
+@app.route('/choose')
+def choose_house():
+    char = request.args.get('character')
+    response = make_response(render_template(
+        'choose_house.html', houses=houses))
+    response.set_cookie('selecting_char', char)
+    return response
+
+
+@app.route('/choose/<house>')
+def choose_character(house):
     URL = 'https://hp-api.onrender.com/api/characters/house/' + house
     res = req.get(URL)
     characters = res.json()
-    return render_template('house.html', house=houses[house], characters=characters)
+    return render_template('choose_character.html', house=houses[house], characters=characters)
 
 
-@app.route('/character/<id>')
-def character(id):
+@app.route('/select/<id>')
+def select(id):
     URL = 'https://hp-api.onrender.com/api/character/' + id
     res = req.get(URL)
     character = res.json()[0]
-    return render_template('character.html', character=character)
+    print(character)
+    response = make_response(redirect(url_for('home')))
+    if request.cookies.get('selecting_char') == '1':
+        response.set_cookie('char_1_name', character['name'])
+        response.set_cookie('char_1_house', character['house'].lower())
+    else:
+        response.set_cookie('char_2_name', character['name'])
+        response.set_cookie('char_2_house', character['house'].lower())
+    response.delete_cookie('selecting_char')
+    return response
+
+
+@app.route('/clear')
+def clear():
+    response = make_response(redirect(url_for('home')))
+    cookies = ['char_1_name', 'char_1_house', 'char_2_name', 'char_2_house']
+    for cookie in cookies:
+        response.delete_cookie(cookie)
+    return response
 
 
 if __name__ == '__main__':
